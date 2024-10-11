435bf84baaefc1890f175db0720320f44bd809fb
Fish <109036036+Peixe2b@users.noreply.github.com>
2024-10-11 13:50:51 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from src.pwe import (
-    PWE_FALSE, PWE_TRUE, PWE_NUMBER   
+    PWE_FALSE, PWE_TRUE, PWE_NUMBER,
+    PWELogger, PWE_VERSION
 )
 from src.pwe import (
     PWE_Init, PWE_Terminate
