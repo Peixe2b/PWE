d3dac70965f8dde79062be5707c276d5bd0e0383
Fish <109036036+Peixe2b@users.noreply.github.com>
2024-10-12 12:28:48 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,5 +3,8 @@ from src.pwe import (
     PWELogger, PWE_VERSION
 )
 from src.pwe import (
-    PWE_Init, PWE_Terminate
+    PWE_Init, PWE_Terminate,
+    PWE_CreateWindow, PWE_DestroyWindow,
+    PWE_SetFullscreen, PWE_SetWindowSize, PWE_SetWindowTitle,
+    PWE_WindowShouldClose
 )
