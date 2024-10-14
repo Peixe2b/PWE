13c17074d799cf8cdafbac0e526e1f4f3f36ddde
Fish <109036036+Peixe2b@users.noreply.github.com>
2024-10-14 16:36:12 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,8 @@ from src.pwe import (
 from src.pwe import (
     PWE_Init, PWE_Terminate,
     PWE_CreateWindow, PWE_DestroyWindow,
-    PWE_WindowShouldClose, PWE_PollEvents,
+    PWE_WindowShouldClose, PWE_PollEvents, PWE_UpdateWindow,
+    PWE_CreateRenderer, PWE_RenderPresent,
     PWEEventController
 )
 from src.pwe_events import *
