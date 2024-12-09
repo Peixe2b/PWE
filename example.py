2c1db9625501ec359e199b028ba002df3ffa4a20
Peixe2b <jclodoaldosantana204@gmail.com>
2024-12-09 18:25:32 -0300

    Basic commit

--- example.py
+++ example.py
@@ -4,10 +4,15 @@ from src import *
 PWE_Init()
 window = PWE_CreateWindow("Game1", 800, 500)
 
-while PWE_WindowShouldClose(window):
-    events = PWEEventController()
+try:
+    while PWE_WindowShouldClose(window):
+        events = PWEEventController()
+        PWELogger.show_warning("WINDOW UPDATE")
 
-    PWELogger.show_warning("WINDOW UPDATE")
-    # PWE_UpdateWindow(window) -> Update window screen
+        while(PWE_PollEvents(events)):
+            pass
 
-PWE_Terminate()
+        # PWE_UpdateWindow() -> Update window screen
+except:
+    PWE_Terminate()
+
