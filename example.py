bbc163d72c47efccffa05d61486ea55ae5f24bf4
Fish <109036036+Peixe2b@users.noreply.github.com>
2024-12-09 14:33:47 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,21 +1,13 @@
+from platform import system
 from src import *
 
 PWE_Init()
 window = PWE_CreateWindow("Game1", 800, 500)
-renderer = PWE_CreateRenderer(window)
-PWE_RenderPresent(renderer)
 
 while PWE_WindowShouldClose(window):
     events = PWEEventController()
 
     PWELogger.show_warning("WINDOW UPDATE")
-    while PWE_PollEvents(events):
-        if events.type == PWE_KEY_SPACE:
-            PWELogger.show_warning("Space pressed")
-        if events.type == PWE_KEY_ESC:
-            PWELogger.show_warning("Close window")
-            window.closed = True
-    PWE_UpdateWindow(window)
+    # PWE_UpdateWindow(window) -> Update window screen
 
-PWE_DestroyWindow(window)
 PWE_Terminate()
