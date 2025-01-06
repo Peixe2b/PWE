1e28ae65ae7891faf4b55af13eedac081d9ee3c7
Fish <109036036+Peixe2b@users.noreply.github.com>
2025-01-06 20:25:57 -0300

    Update __init__.py

--- PWE/__init__.py
+++ PWE/__init__.py
@@ -1,28 +1,8 @@
-""" The module provides the following functions:
-    PWE_Init: Initializes the PWE library.
-    PWE_Terminate: Terminates the PWE library.
-    PWE_CreateWindow: Creates a window.
-    PWE_UpdateWindow: Updates the window.
-    PWE_WindowShouldClose: Checks if the window should be closed.
-    PWE_PollEvents: Polls for events.
-    PWE_GetSurface: Retrieves the surface of the window.
-    And More...
-
-    Note: The PWE library must be installed and linked before using these functions.
-
-    PWE framework version: 1.0.0 is used for create a game using SDL.
-    PWE create a connection to SDL using the "PWE_Init" function.
-
-    PWE is free software released under the Apache license. You can use, 
-    modify, and distribute this software freely, don't have to ask permission.
-"""
-
-
 from PWE.main import main
 from PWE.pwe import (
     PWE_Init, PWE_Terminate,
     PWE_CreateWindow, PWE_UpdateWindow,
-    PWE_WindowShouldClose, PWE_PollEvents,
-    PWE_GetSurface, PWEEventController
+    PWE_PollEvents, PWE_GetSurface,
+    PWEEventController
 )
 from PWE._pwe_events import *
