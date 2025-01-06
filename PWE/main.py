515dcad450fe260ab4bbb722a0197af92c75ea20
Fish <109036036+Peixe2b@users.noreply.github.com>
2025-01-06 20:26:35 -0300

    Update main.py

--- PWE/main.py
+++ PWE/main.py
@@ -1,10 +1,9 @@
 from platform import system
-from typing import TypeAlias, Union
+from typing import TypeAlias, Any
 from ctypes import c_int
 
 
 # All system config for initialize
-sdl = None
 PWE_SYSTEM = system()
 PWE_PLATFORM_LINUX: str = "Linux"
 PWE_PLATFORM_DARWIN: str = "Darwin"
@@ -14,9 +13,9 @@ PWE_DARWIN_SDL = "libSDL2.dylib"
 PWE_LINUX_SDL = "libSDL2.so" 
 
 # Generate alias type vars
-PWE_TRUE = 1
-PWE_FALSE = 0
-PWE_NUMBER: TypeAlias = Union[c_int, int]
+PWE_TRUE = True
+PWE_FALSE = False
+PWE_NUMBER: TypeAlias = c_int
 PWE_FPS = 60
 
 # Generate alias type FLAGS
@@ -26,9 +25,17 @@ PWE_INIT_VIDEO = 0x00000020
 PWE_INIT_AUDIO = 0x00000010
 PWE_INIT_EVENTS = 0x00004000
 
+# Singletons classes
+class SDLSingleton:
+    def __init__(self): self.__sdl = None
+    def set_sdl(self, value: Any): self.__sdl = value
+    @property
+    def SDL(self): return self.__sdl
+
 # PWE Pointers for classes
 
 # PWE Singletons
+sdl_singleton = SDLSingleton()
 
 def main():
     # System config
