fa52fd85c98f242a79c5328c557cc5904c252f63
Fish <109036036+Peixe2b@users.noreply.github.com>
2025-01-06 20:25:35 -0300

    Update pwe.py

--- PWE/pwe.py
+++ PWE/pwe.py
@@ -3,7 +3,6 @@ from typing import (
 )
 from ctypes import c_int, c_void_p, c_char_p
 
-
 from PWE.main import *
 from PWE.config import *
 from PWE._pwe_datatypes import * 
@@ -11,9 +10,10 @@ from PWE._pwe_events import *
 from PWE._pwe_errors import PWEBasicError, PWETypeError, PWEPlatformError
 from PWE.core.utils import *
 
+main() # Setting all engine
 
-def PWE_Init() -> Literal[True, False]: # Initialize SDL 
-    global sdl
+def PWE_Init() -> Literal[True, False]: # Initialize SDL and ALL system
+    sdl = None
     system_is_valid: Literal[True, False] = check_platform()
 
     if system_is_valid != PWE_TRUE:
@@ -27,20 +27,22 @@ def PWE_Init() -> Literal[True, False]: # Initialize SDL
         sdl = open_sdl_library(PWE_DARWIN_SDL)
     elif system_info[1] == PWE_PLATFORM_WINDOWS:
         sdl = open_sdl_library(PWE_WINDOW_SDL)
-    init_or_quit_sdl(PWE_INITIALIZE, sdl)
+    
+    sdl_singleton.set_sdl(sdl)
+    init_or_quit_sdl(PWE_INITIALIZE, sdl) # Initialize SDL library
     return PWE_TRUE
 
 
 def PWE_Terminate() -> None:
-    init_or_quit_sdl(PWE_QUIT, sdl)
+    init_or_quit_sdl(PWE_QUIT, sdl_singleton.SDL)
 
 
 def PWE_CreateWindow(title: str, width: int, height: int) -> Union[PWEWindow, None]:
-    sdl.SDL_CreateWindow.argtypes = [
+    sdl_singleton.SDL.SDL_CreateWindow.argtypes = [
         c_char_p, c_int, c_int, c_int,
         c_int, c_int
     ]
-    sdl.SDL_CreateWindow.restype = c_void_p
+    sdl_singleton.SDL.SDL_CreateWindow.restype = c_void_p
 
     if width < 0 or height < 0:
         PWELogger.show_error("Window dimensions must be positive", PWETypeError)
@@ -55,28 +57,22 @@ def PWE_CreateWindow(title: str, width: int, height: int) -> Union[PWEWindow, No
     return window
 
 
-def PWE_WindowShouldClose(window: PWEWindow) -> Literal[True, False]:
-    if window.closed == PWE_TRUE:
-        return PWE_TRUE
-    return PWE_FALSE
-
-
 def PWE_GetSurface(window: PWEWindow) -> Any:
-    sdl.SDL_GetWindowSurface.argtypes = [c_void_p]
-    sdl.SDL_GetWindowSurface.restype = c_void_p
+    sdl_singleton.SDL.SDL_GetWindowSurface.argtypes = [c_void_p]
+    sdl_singleton.SDL.SDL_GetWindowSurface.restype = c_void_p
 
     if window.handle is not None:    
-        surface = sdl.SDL_GetWindowSurface(window.handle)
+        surface = sdl_singleton.SDL.SDL_GetWindowSurface(window.handle)
         return surface
     return None
 
 
 def PWE_UpdateWindow(window: PWEWindow) -> Union[PWEBasicError, Any]:
-    sdl.SDL_UpdateWindowSurface.argtypes = [c_void_p]
-    sdl.SDL_UpdateWindowSurface.restype = c_void_p
+    sdl_singleton.SDL.SDL_UpdateWindowSurface.argtypes = [c_void_p]
+    sdl_singleton.SDL.SDL_UpdateWindowSurface.restype = c_void_p
 
     try:
-        sdl.SDL_UpdateWindowSurface(window.handle)
+        sdl_singleton.SDL.SDL_UpdateWindowSurface(window.handle)
     except PWEBasicError as e:
         PWELogger.show_error(f"Failed to update window: {window.title}, because {e}", PWEBasicError)
         return PWEBasicError
